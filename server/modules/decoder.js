de535b7323079739fd8a9616b582da15e82327f2
Lopan455 <tjohander@gmail.com>
2017-05-04 16:24:20 -0500

    changed FB auth settings

--- server/modules/decoder.js
+++ server/modules/decoder.js
@@ -1,8 +1,18 @@
 var admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert("./server/firebase-service-account.json"),
-  databaseURL: "https://lets-do-this-3d411.firebaseio.com" // replace this line with your URL
+  credential: admin.credential.cert({
+    "type": process.env.FIREBASE_SERVICE_ACCOUNT_TYPE,
+    "project_id": process.env.FIREBASE_SERVICE_ACCOUNT_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY,
+    "client_email": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_URI,
+    "token_uri": process.env.FIREBASE_SERVICE_ACCOUNT_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL}),
+    databaseURL: "https://lets-do-this-3d411.firebaseio.com"
 });
 
 /* This is where the magic happens. We pull the id_token off of the request,
